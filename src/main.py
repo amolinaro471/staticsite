e704df62ec1e52bc9a2fb4bc68917ef16cc635a8
amolinaro471 <andrew.molinaro916@gmail.com>
2024-04-28 22:55:20 -0500

    commiting what I have so far

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode
+#print("hello world")
+
+def main():
+    test = TextNode("this is text", "bold", "https://www.boot.dev")
+    print(test)
+
+main()
